16c649aff785e3d590991701557767c664852f1c
Denys Vuika <denys.vuika@gmail.com>
2020-11-12 13:17:48 +0000

    add compare tests

--- src/utils.rs
+++ src/utils.rs
@@ -13,3 +13,17 @@ pub fn compare<T: Ord>(a: &[T], b: &[T]) -> Ordering {
     }
     return a.len().cmp(&b.len());
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn compare_works_as_expected() {
+        assert_eq!(Ordering::Equal, compare(&[1, 2, 3], &[1, 2, 3]));
+        assert_eq!(Ordering::Less, compare(&[1, 0], &[1, 2]));
+        assert_eq!(Ordering::Less, compare(&[], &[1, 2, 3]));
+        assert_eq!(Ordering::Greater, compare(&[1, 2, 3], &[1, 2]));
+        assert_eq!(Ordering::Greater, compare(&[1, 3], &[1, 2]));
+    }
+}
